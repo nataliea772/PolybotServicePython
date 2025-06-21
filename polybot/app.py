16e49d2672cdf12b2bb5c76887dfc10d21ddd2f4
nataliea772 <natalie22ad@gmail.com>
2025-06-21 23:13:25 +0300

    fixed deployment

--- polybot/app.py
+++ polybot/app.py
@@ -37,7 +37,4 @@ def webhook():
 bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, BOT_APP_URL)
 
 if __name__ == "__main__":
-    # bot.setWebhook(f"{BOT_APP_URL}/{TELEGRAM_BOT_TOKEN}/")  #This tells Telegram where to send updates
-
-    # app.run(host='0.0.0.0', port=8443)
     app.run(host='0.0.0.0', port=8080)
